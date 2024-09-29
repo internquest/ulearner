d54418980cf7acb5bd89c88c0b785b87b0bcebbc
shiva kumar <98257507+Shivuduu19@users.noreply.github.com>
2024-09-29 15:45:30 +0530

    add pagination buttons svgs

--- app/components/Videos.jsx
+++ app/components/Videos.jsx
@@ -189,32 +189,32 @@ const Videos = ({ videos, videolink, playlistlink }) => {
                 </span>
                 <button
                     onClick={() => paginate(1)}
-                    className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-700"
+                    className="mx-1 px-3 py-1 rounded bg-[#030712] disabled:text-[#4b5563] disabled:bg-[#11182799] text-[#f9fafb]"
                     disabled={currentPage === 1}
                 >
-                    First
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-hidden="true" class="remixicon size-4 shrink-0"><path d="M4.83578 12L11.0429 18.2071L12.4571 16.7929L7.66421 12L12.4571 7.20712L11.0429 5.79291L4.83578 12ZM10.4857 12L16.6928 18.2071L18.107 16.7929L13.3141 12L18.107 7.20712L16.6928 5.79291L10.4857 12Z"></path></svg>
                 </button>
                 <button
                     onClick={() => paginate(currentPage - 1)}
-                    className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-700"
+                    className="mx-1 px-3 py-1 rounded  bg-[#030712] disabled:text-[#4b5563] disabled:bg-[#11182799] text-[#f9fafb]"
                     disabled={currentPage === 1}
                 >
-                    Previous
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-hidden="true" class="remixicon size-4 shrink-0"><path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path></svg>
                 </button>
 
                 <button
                     onClick={() => paginate(currentPage + 1)}
-                    className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-700"
+                    className="mx-1 px-3 py-1 rounded  bg-gray-200 bg-[#030712] disabled:text-[#4b5563] disabled:bg-[#11182799] text-[#f9fafb]"
                     disabled={currentPage === totalPages}
                 >
-                    Next
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-hidden="true" class="remixicon size-4 shrink-0"><path d="M13.1717 12.0007L8.22192 7.05093L9.63614 5.63672L16.0001 12.0007L9.63614 18.3646L8.22192 16.9504L13.1717 12.0007Z"></path></svg>
                 </button>
                 <button
                     onClick={() => paginate(totalPages)}
-                    className="mx-1 px-3 py-1 rounded bg-gray-200 text-gray-700"
+                    className="mx-1 px-3 py-1 rounded  bg-[#030712] disabled:text-[#4b5563] disabled:bg-[#11182799] text-[#f9fafb]"
                     disabled={currentPage === totalPages}
                 >
-                    Last
+                    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" aria-hidden="true" class="remixicon size-4 shrink-0"><path d="M19.1642 12L12.9571 5.79291L11.5429 7.20712L16.3358 12L11.5429 16.7929L12.9571 18.2071L19.1642 12ZM13.5143 12L7.30722 5.79291L5.89301 7.20712L10.6859 12L5.89301 16.7929L7.30722 18.2071L13.5143 12Z"></path></svg>
                 </button>
             </div>
         </div>
